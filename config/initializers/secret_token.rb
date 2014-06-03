935885804887a86621d260470befdd41325e5755
Kavita Jadhav <kavitama@INkavitama.local>
2014-06-03 12:51:47 +0530

    adding,deleting and displaying all todos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SharedTodoApp::Application.config.secret_key_base = '5b36cf8f73790e07c01b8a7eeeb10847c61459693817f1112c28b1b24850cff1de97dcca63f0f76de3aa9052f167d01a494d40aec2a36aabb569e95c3211c3a4'
